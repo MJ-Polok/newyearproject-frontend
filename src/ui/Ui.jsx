e3d5902a018b388f7ba148d4134eb2936baf97a5
MJ Polok <mjpolok018@gmail.com>
2024-12-29 00:46:32 +0600

    more responsive for height

--- src/ui/Ui.jsx
+++ src/ui/Ui.jsx
@@ -5,24 +5,24 @@ import { Toaster } from "react-hot-toast";
 
 const Ui = () => {
     return (
-        <div className="mainDiv bg-cyan-200 h-screen overflow-hidden">
+        <div className="bg-cyan-200 h-screen overflow-hidden mainDiv">
             <Toaster/>
-            <div className="box-border bg-[#6172db] h-[55.5%] p-[40px] pt-[40px] flex justify-center font-year text-[#ffa24d] ">
-                <div className="year w-full flex text-[9rem] flex-col justify-center items-center">
+            <div className="box-border flex justify-center bg-[#6172db] p-[40px] pt-[40px] h-[55.5%] font-year text-[#ffa24d] yearbox">
+                <div className="flex flex-col justify-center items-center w-full text-[9rem] year">
                     2025
-                    <span className="fr relative opacity-30 left-[131px] bottom-[215px]">4</span>
+                    <span className="relative bottom-[215px] left-[131px] opacity-30 df f fr">4</span>
                 </div>
             </div>
-            <div className="bg-[#2c483e] h-[42%] flex font-texts leading-[35px] text-[#ffa24d]">
-                <div className="leftSide h-full w-[38%] p-3 pt-10 text-[4.7rem]">
+            <div className="flex bg-[#2c483e] h-[42%] font-texts text-[#ffa24d] leading-[35px]">
+                <div className="leftSide p-3 pt-10 w-[38%] h-full text-[4.7rem]">
                     <div className="text">
                         <p>Happy</p>
                         <p>New</p>
                         <p>Year!</p>
                     </div>
                 </div>
-                <div className="rightSide h-full w-[62%] pb-11 pr-2 text-[3.8rem]">
-                    <div className="text flex flex-col-reverse h-full">
+                <div className="rightSide pr-2 pb-11 w-[62%] h-full text-[3.8rem]">
+                    <div className="flex flex-col-reverse h-full text">
                         I hope many
                         many good
                         news is waiting
@@ -31,7 +31,7 @@ const Ui = () => {
                 </div>
             </div>
             <FloatSection />
-            <div className="footer bg-[#e95436] h-[2.5%] text-sm font-footer flex justify-center font-bold text-[17px] leading-3 items-center text-[#2F2F2F] ">
+            <div className="flex justify-center items-center bg-[#e95436] h-[2.5%] font-bold font-footer text-[#2F2F2F] text-[17px] text-sm leading-3 footer">
                 <p className="opacity-60">Copyright©MJ Polok</p>
             </div>
 
@@ -39,13 +39,13 @@ const Ui = () => {
                 <div className="modal-box">
                     <form method="dialog">
                         {/* if there is a button in form, it will close the modal */}
-                        <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
+                        <button className="top-2 right-2 absolute btn btn-circle btn-ghost btn-sm">✕</button>
                     </form>
                     <h3 className="font-bold text-lg">Hello! I am MJ Polok.</h3>
                     <p className="py-4 pb-2">This is a fun New Year project.</p>
                     <p className="py-3 pb-1">The design of this project came from this photo:</p>
                     <img src={newyearpic} alt="" />
-                    <p className="py-4">You can check this on my GitHub: <Link className="link text-[#e95436]" to="https://github.com/MJ-Polok/newyearproject-frontend"><i>Go --></i></Link></p>
+                    <p className="py-4">You can check this on my GitHub: <Link className="text-[#e95436] link" to="https://github.com/MJ-Polok/newyearproject-frontend"><i>Go --></i></Link></p>
                     
                     <i></i>
                 </div>
